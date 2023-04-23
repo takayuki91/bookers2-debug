bada0c80e31877037f7d4ae9923f258cae4f523f
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-23 14:30:59 +0000

    [Add]comment model controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-
-
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
 end
