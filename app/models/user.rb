94992c1854e19b97ce762610e4b828d184903919
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-28 01:56:10 +0000

    [Add]search controller&model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,30 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  # フォローする側からのhas_manyであることをforeign_keyで明示
+  # フォローする側からのhas_many # 全員分のデータ取得
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  # 全員分のデータを取ってくるため
   has_many :followers, through: :relationships, source: :followed
-  
-  # フォローされる側からのhas_manyであることをforeign_keyで明示
+  # フォローされる側からのhas_many # 全員分のデータ取得
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # 全員分のデータを取ってくるため
   has_many :followeds, through: :reverse_of_relationships, source: :follower
-  
-  # あるユーザーが(user)にフォローされているか否かを判定するメソッドを定義
+  # あるユーザーが(user)にフォローされているかを判定するメソッド定義
   def is_followed_by?(user)
     reverse_of_relationships.find_by(follower_id: user.id).present?
+  end
+  
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
   end
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
