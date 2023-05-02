cf2811d5ba03686e5d575e4ac0558c6af845e229
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-05-02 09:22:21 +0000

    [Add]jquery comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    @book_comment = BookComment.new
+    # redirect_to request.referer
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    # redirect_to request.referer
   end
 
   private
