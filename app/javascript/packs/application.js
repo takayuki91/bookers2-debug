758b4861c9df6f27d56361e1785eb8f2a8e97fe8
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-06-16 05:08:06 +0000

    [Add]javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// 
+import $ from 'jquery';
 import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+import "../stylesheets/about"
 
 Rails.start()
 Turbolinks.start()
