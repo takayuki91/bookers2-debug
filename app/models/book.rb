5735cd65ea23491e63565b196eec0e4e438b1d63
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-02 01:08:05 +0000

    [Add]favorites sort in descending order

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,17 +2,25 @@ class Book < ApplicationRecord
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   validates :category, presence: true
-  
+
   # レコードを特定の順序で取得
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(star: :desc)}
+
+  scope :popular_books, -> {
+   joins(:favorites)
+   .where(favorites: { created_at: 1.week.ago..Time.now })
+   .group('books.id')
+   .order('COUNT(favorites.id) DESC')
+  }
 
   # favorited_by?メソッド
   def favorited_by?(user)
