e7972c332b777fd721f51e302928b9b40ab03756
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-03 07:52:37 +0000

    [Fix]dm

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,36 @@
 class MessagesController < ApplicationController
-  
-  before_action :authenticate_user!, only: [:create]
+
+  before_action :authenticate_user!, only: [:show, :create]
+
+  def show
+    @user = User.find(params[:id])
+    rooms = current_user.entries.pluck(:room_id)
+    entries = Entry.find_by(user_id: @user.id, room_id: rooms)
+
+    unless entries.nil?
+      @room = entries.room
+    else
+      @room = Room.new(user_id: current_user.id)
+      @room.save
+      Entry.create(user_id: current_user.id, room_id: @room.id)
+      Entry.create(user_id: @user.id, room_id: @room.id)
+    end
+    @messages = @room.messages
+    @message = Message.new(room_id: @room.id)
+  end
 
   def create
-    if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
-    else
-      flash[:alert] = "メッセージ送信に失敗しました。"
-    end
-　　redirect_to "/rooms/#{@message.room_id}"
+    @message = current_user.messages.new(message_params)
+    @message.save
+    redirect_to request.referer
   end
-  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message, :room_id)
+  end
+
+
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,26 +7,26 @@ class UsersController < ApplicationController
     @books = @user.books
     @book = Book.new
     
-    # DM関係
-    @currentUserEntry = Entry.where(user_id: current_user.id)
-    @userEntry = Entry.where(user_id: @user.id)
+    # # DM関係
+    # @currentUserEntry = Entry.where(user_id: current_user.id)
+    # @userEntry = Entry.where(user_id: @user.id)
     
-    if @user.id == current_user.id
-    else
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
+    # if @user.id == current_user.id
+    # else
+    #   @currentUserEntry.each do |cu|
+    #     @userEntry.each do |u|
+    #       if cu.room_id == u.room_id then
+    #         @isRoom = true
+    #         @roomId = cu.room_id
+    #       end
+    #     end
+    #   end
+    #   if @isRoom
+    #   else
+    #     @room = Room.new
+    #     @entry = Entry.new
+    #   end
+    # end
     
   end
 
