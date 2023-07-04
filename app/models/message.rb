ed7f37644030229d0f027499040a47d95bb0c9a1
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-04 09:01:36 +0000

    [Add]user.rb validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
-  
-  validates :message, length: { maximum: 140 }
+
+  validates :message, presence: true, length: { maximum: 140 }
   belongs_to :user
   belongs_to :room
-  
+
 end
