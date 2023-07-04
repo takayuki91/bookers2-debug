cb9f8b7a43e5b33e83dc62437966ccf599c5da1b
takayuki91 <takayuki.nakatsuji@gmail.com>
2023-07-04 01:16:23 +0000

    [Add]book record count

--- app/models/book.rb
+++ app/models/book.rb
@@ -15,7 +15,8 @@ class Book < ApplicationRecord
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(star: :desc)}
-
+  
+  # いいねの多い順
   scope :popular_books, -> {
    joins(:favorites)
    .where(favorites: { created_at: 1.week.ago..Time.now })
@@ -32,6 +33,12 @@ class Book < ApplicationRecord
   def user_has_look?(user)
     looks.where(user_id: user.id).exists?
   end
+  
+  # 投稿数カウント
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
   # 検索方法分岐
   def self.looks(search, word)
